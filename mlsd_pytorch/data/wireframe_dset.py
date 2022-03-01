ea576faaad7ebe3b15b7126904e6377f3045f8d4
Michele Bechini <michele.bechini@polimi.it>
2022-03-01 18:10:15 +0100

    changed train augmentation

--- mlsd_pytorch/data/wireframe_dset.py
+++ mlsd_pytorch/data/wireframe_dset.py
@@ -15,7 +15,16 @@ from albumentations import (
     OneOf,
     HueSaturationValue,
     Compose,
-    Normalize
+    Normalize,
+    Flip,
+    Rotate,
+    Affine,
+    RandomScale,
+    Downscale,
+    ColorJitter,
+    GaussianBlur,
+    GaussNoise,
+    KeypointParams
 )
 
 from mlsd_pytorch.data.utils import \
@@ -106,7 +115,7 @@ class Line_Dataset(Dataset):
 
 
         self.input_size = cfg.datasets.input_size
-        self.train_aug = self._aug_train()
+        self.train_aug = self._aug_train() # define the function for train_set augmentation
         self.test_aug = self._aug_test(input_size=self.input_size)
 
         self.cache_to_mem =  cfg.train.cache_to_mem
@@ -121,29 +130,45 @@ class Line_Dataset(Dataset):
         return len(self.anns)
 
     def _aug_train(self):
-        aug = Compose(
-            [
-                OneOf(
-                    [
-                        HueSaturationValue(hue_shift_limit=10,
-                                           sat_shift_limit=10,
-                                           val_shift_limit=10,
-                                           p=0.5),
-                        RandomBrightnessContrast(brightness_limit=0.2,
-                                                 contrast_limit=0.2,
-                                                 p=0.5)
-                    ]
-                ),
-                #                 OneOf(
-                #                     [
-                #                         Blur(blur_limit=3, p=0.5),
-                #                         GaussianBlur(blur_limit=3, p=0.5),
-                #                         MedianBlur(blur_limit=3, p=0.5)
-                #                     ]
-                #                 ),
+        # TODO: tune the noises for the image
+        aug = Compose([
+            Flip(p=0.5),  # random flip vertically and/or horizontally the image
+            Rotate(limit=90, interpolation=3, border_mode=1, p=0.5), # rotate with 90° limits with Area interpolation and border reflect
+            Affine(shear=[-45, 45], interpolation=3, fit_output=True, mode=1, p=0.5), # shear with area interpolation and border reflect
+            OneOf([
+                RandomScale(scale_limit=0.5, interpolation=3, p=0.5), # resize the image to smaller (0.5) or bigger (1.5) size
+                Downscale(scale_min=0.25, scale_max=0.75, interpolation=3, p=0.5)
+                ], p=0.5),
+            ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0., p=0.5),
+            GaussianBlur(sigma_limit=75, always_apply=True),
+            GaussNoise(var_limit=(10, 50), mean=0, always_apply=True)
+        ], p=1, keypoint_params=KeypointParams(format='xy', remove_invisible=False)) # add the keyword for keypoints
 
-            ],
-            p=1.0)
+
+        # OLD Version
+        # aug = Compose(
+        #     [
+        #         OneOf(
+        #             [
+        #                 HueSaturationValue(hue_shift_limit=10,
+        #                                    sat_shift_limit=10,
+        #                                    val_shift_limit=10,
+        #                                    p=0.5),
+        #                 RandomBrightnessContrast(brightness_limit=0.2,
+        #                                          contrast_limit=0.2,
+        #                                          p=0.5)
+        #             ]
+        #         ),
+        #         #                 OneOf(
+        #         #                     [
+        #         #                         Blur(blur_limit=3, p=0.5),
+        #         #                         GaussianBlur(blur_limit=3, p=0.5),
+        #         #                         MedianBlur(blur_limit=3, p=0.5)
+        #         #                     ]
+        #         #                 ),
+        #
+        #     ],
+        #     p=1.0)
         return aug
     
     def _aug_test(self, input_size=384):
